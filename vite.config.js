a8e0590821c44dfa2e82a6896bca648506b4e75d
miqi <445112762@qq.com>
2023-04-02 17:07:26 +0800

    旅馆项目代码

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite';
-import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [VantResolver()],
-      // libs: [
-      //   {
-      //     libraryName: 'vant',
-      //     esModule: true,
-      //     resolveStyle: name => `../es/${name}/style`
-      //   }
-    //  ]
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
